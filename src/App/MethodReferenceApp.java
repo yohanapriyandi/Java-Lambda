162bc795771dadb8b6a72ba3af1c66f412ba8986
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-01-22 20:39:48 +0700

    Method reference static dan non-static

--- src/App/MethodReferenceApp.java
+++ src/App/MethodReferenceApp.java
@@ -16,13 +16,35 @@ public class MethodReferenceApp {
         // Lambda style
         // Predicate<String> predicateToLowercase = value -> StringUtil.isLowerCase(value);
 
-        // Lamda using method reference
+        // Lamda using static method reference
         Predicate<String> predicateToLowercase = StringUtil::isLowerCase;
 
         System.out.println(predicateToLowercase.test("Eko"));
         System.out.println(predicateToLowercase.test("eko"));
         System.out.println(predicateToLowercase.test("EKO"));
         System.out.println(predicateToLowercase.test("EKo"));
+    }
 
+    public void run(){
+        /**
+         * Lamda using non  static method reference
+         * apabila ingin mengakses object dari luar maka dekalarasikan objeknya
+         * kemudian panggil nama objeknya
+         */
+
+        // MethodReferenceApp app = new MethodReferenceApp();
+
+        Predicate<String> predicateToLowerCase = this::isLowerCase;
+        System.out.println(predicateToLowerCase.test("Eko"));
+        System.out.println(predicateToLowerCase.test("eko"));
+    }
+
+    public  boolean isLowerCase(String value){
+        for (var c : value.toCharArray()){
+            if (!Character.isLowerCase(c)){
+                return false;
+            }
+        }
+        return true;
     }
 }
